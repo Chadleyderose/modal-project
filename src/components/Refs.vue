35a0253cf67ba8afce6656046c97dea160dde725
Chadley De Rose <Chadleyderose1911201@gmail.com>
2021-07-23 09:49:19 +0200

    Pushing-1

--- src/components/Refs.vue
+++ src/components/Refs.vue
@@ -1,16 +1,16 @@
 <template>
-<h1>{{title}}</h1>
-<Modal/>
+<input type="text" ref="name">
+<button @click="handleClick">click me </button>
 </template>
 
 <script>
-import Modal from'./components/Modal'
 export default {
   name: 'App',
-  components:{Modal},
-  data(){
-    return{   
-    title: 'My First Vue App :)'
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
     }
   }
 }
@@ -24,10 +24,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-h1{
-   border-bottom: 1px solid #ddd;
-   display: inline-block;
-   padding-bottom: 10px;
 }
 </style>
